6cf22b918edbb70f30c5e3de751d78a226cf4be3
Yuganshu <yuganshu219@gmail.com>
2024-06-23 10:06:05 +0530

    firebase config modified

--- firebase.js
+++ firebase.js
@@ -1,14 +1,7 @@
 import { initializeApp, getApps, getApp } from 'firebase/app';
 import { getFirestore } from 'firebase/firestore';
 
-const firebaseConfig = {
-    apiKey: process.env.FIREBASE_API_KEY,
-    authDomain: process.env.ENV.FIREBASE_AUTH_DOMAIN,
-    projectId: process.env.FIREBASE_PROJECT_ID,
-    storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
-    messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
-    appId: process.env.FIREBASE_APP_ID
-};
+const firebaseConfig = { "apiKey": "AIzaSyCXbDlk8woQbdPgQfuo2hLKlzydL_50tRg", "authDomain": "chat-gpt-clone-ee343.firebaseapp.com", "projectId": "chat-gpt-clone-ee343", "storageBucket": "chat-gpt-clone-ee343.appspot.com", "messagingSenderId": "388310070736", "appId": "1:388310070736:web:a5cfeecf29053ff38175d5" }
 
 const app = getApps().length ? getApp() : initializeApp(firebaseConfig);
 const db = getFirestore(app);
